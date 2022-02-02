4d47d9a895439248bce28bc322c71cbde5f1a75f
Marc <marcjacques@me.com>
2022-02-01 21:44:32 -0500

    Add feature to check if Square is occupied

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -7,11 +7,17 @@
 
 import SwiftUI
 
-let columns: [GridItem] = [GridItem(.flexible()),
-                           GridItem(.flexible()),
-                           GridItem(.flexible()),]
+
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible()),]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -24,10 +30,15 @@ struct ContentView: View {
                                 .frame(width: geometry.size.width/3 - 15,
                                        height: geometry.size.width/3 - 15)
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) { return }
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                            isHumanTurn.toggle()
                         }
                     }
                 }
@@ -35,6 +46,10 @@ struct ContentView: View {
             }
             .padding()
         }
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index })
     }
 }
 
