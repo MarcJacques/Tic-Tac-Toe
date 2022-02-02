60e6979724412d419345e44d33005fc709c17312
Marc <marcjacques@me.com>
2022-02-01 21:26:03 -0500

    Add Circles for ticTacToe

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+let columns: [GridItem] = [GridItem(.flexible()),
+                           GridItem(.flexible()),
+                           GridItem(.flexible()),]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns) {
+                    ForEach(0..<9) {i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(.cyan).opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 15,
+                                       height: geometry.size.width/3 - 15)
+                        }
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
